735d8979b8e166b0fd7136337c7dd64959c5780d
Alttaab <alttaab@gmail.com>
2022-11-25 12:46:07 -0800

    Creating schemas for databases: medical center, soccer league, and craig's list. Additionally, refactoring schemas for music, outer space, and air traffic databases.

---
+++ sql-ddl-design/medical_center.sql
@@ -0,0 +1,48 @@
+﻿DROP DATABASE IF EXISTS  medical_center_db;
+
+CREATE DATABASE medical_center_db;
+
+\c medical_center_db
+
+CREATE TABLE patients (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    billind_address TEXT NOT NULL,
+    insurance TEXT NOT NULL
+);
+
+CREATE TABLE specialties (
+    id SERIAL PRIMARY KEY,
+    medical_specialty TEXT NOT NULL
+);
+
+CREATE TABLE doctors (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    medical_specialty INTEGER REFERENCES Specialties (id) NOT NULL
+);
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    visit_date DATE NOT NULL,
+    patient INTEGER REFERENCES patients (id) NOT NULL,
+    doctor INTEGER REFERENCES doctors (id) NOT NULL,
+    notes TEXT NULL
+);
+
+CREATE TABLE diagnosis (
+    id SERIAL PRIMARY KEY,
+    notes TEXT NULL,
+    visit INTEGER REFERENCES visits (id) NOT NULL,
+    disease INTEGER REFERENCES diseases (id) NOT NULL
+);
+
+CREATE TABLE diseases (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    medical_specialty INTEGER REFERENCES specialties (id) NULL,
+    description TEXT NOT NULL
+);
+
---
+++ sql-ddl-design/soccer_league.sql
@@ -0,0 +1,53 @@
+﻿DROP DATABASE IF EXISTS  soccer_league_db;
+
+CREATE DATABASE soccer_league_db;
+
+\c soccer_league_db
+
+CREATE TABLE seasons (
+  id SERIAL PRIMARY KEY,
+  start_date DATE NOT NULL,
+  end_date DATE NULL
+);
+
+CREATE TABLE teams (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE players (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  team INTEGER REFERENCES teams (id) NOT NULL,
+  season INTEGER REFERENCES seasons (id) NOT NULL,
+  number INTEGER NOT NULL
+);
+
+CREATE TABLE matches (
+  id SERIAL PRIMARY KEY,
+  match_date DATE NOT NULL,
+  season INTEGER REFERENCES seasons (id) NOT NULL,
+  home_team INTEGER REFERENCES teams (id) NOT NULL,
+  away_team INTEGER REFERENCES teams (id) NOT NULL,
+  winning_team INTEGER REFERENCES teams (id) NULL
+);
+
+CREATE TABLE goals (
+  id SERIAL PRIMARY KEY,
+  player INTEGER REFERENCES players (id) NOT NULL,
+  match INTEGER REFERENCES matches (id) NOT NULL,
+  notes TEXT NULL
+);
+
+CREATE TABLE referees (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  match INTEGER REFERENCES matches (id) NOT NULL
+);
+
+CREATE TABLE rankings (
+  id serial PRIMARY KEY,
+  team INTEGER REFERENCES teams (id) NOT NULL,
+  season INTEGER REFERENCES seasons (id) NOT NULL,
+  ranking INTEGER NOT NULL
+);
